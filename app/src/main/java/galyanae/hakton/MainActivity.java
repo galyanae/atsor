e562884b0ab69e00f9f0bccdfa650fd6d05be1c4
hadar dabah <hadar.dabah@gmail.com>
2018-05-31 22:57:12 +0300

    add lisineer to gas buttun

--- app/src/main/java/galyanae/hakton/MainActivity.java
+++ app/src/main/java/galyanae/hakton/MainActivity.java
@@ -2,12 +2,21 @@ package galyanae.hakton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button gasB;
+    private Button breaks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        gasB=(Button)findViewById(R.id.gas_button);
+        breaks=(Button)findViewById(R.id.breaks_buutton);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gasB.setOnClickListener(new onclick_gas());
     }
+
+
 }
---
+++ app/src/main/java/galyanae/hakton/onclick_gas.java
@@ -0,0 +1,11 @@
+package galyanae.hakton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class onclick_gas extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+
+    }
+}
